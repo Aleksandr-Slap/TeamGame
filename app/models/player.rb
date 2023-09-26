ceb15bb0c63749dfc8dc86fc5888c87849c3a527
KALi <aleksandr-slap@rambler.ru>
2023-09-26 18:03:09 +0300

    added two instance methods

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,12 @@ class Player < ApplicationRecord
   has_many :statistics
   has_many :matches, through: :statistics
   belongs_to :team, optional: true
+
+  def assign_distance(match_id, distance)
+    Statistic.where(match_id: match_id, player_id: self.id).update(distance: distance)
+  end
+
+  def assign_accuracy(match_id, accuracy)
+    Statistic.where(match_id: match_id, player_id: self.id).update(accuracy: accuracy)
+  end 
 end
